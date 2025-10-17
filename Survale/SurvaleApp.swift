33e5da3b0249232e31d18f8a8b08dccab407c927
Sean Fillmore <sean@fillmoreecommercesolutions.com>
2025-10-17 13:28:24 -0700

    Initial Commit

---
+++ Survale/SurvaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurvaleApp.swift
+//  Survale
+//
+//  Created by Sean Fillmore on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SurvaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
