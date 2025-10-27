ad354b253dfa3086bc8d226d8d5063c14a217bd0
Sean Fillmore <sean@fillmoreecommercesolutions.com>
2025-10-27 13:41:38 -0700

    Fix: Remove duplicate Color(hex:) extension
    The Color(hex:) extension was already defined in AssignLocationSheet.swift.
    Removed duplicate from ActiveOperationDetailView.swift to fix build error.
    
    Error: Invalid redeclaration of 'init(hex:)'
    Status: Fixed ✅

--- Views/ActiveOperationDetailView.swift
+++ Views/ActiveOperationDetailView.swift
@@ -1384,36 +1384,5 @@ struct MemberRow: View {
 }
 
 // MARK: - Color Extension
-
-extension Color {
-    init?(hex: String) {
-        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-        
-        var rgb: UInt64 = 0
-        
-        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else {
-            return nil
-        }
-        
-        let length = hexSanitized.count
-        
-        let r, g, b, a: Double
-        if length == 6 {
-            r = Double((rgb & 0xFF0000) >> 16) / 255.0
-            g = Double((rgb & 0x00FF00) >> 8) / 255.0
-            b = Double(rgb & 0x0000FF) / 255.0
-            a = 1.0
-        } else if length == 8 {
-            r = Double((rgb & 0xFF000000) >> 24) / 255.0
-            g = Double((rgb & 0x00FF0000) >> 16) / 255.0
-            b = Double((rgb & 0x0000FF00) >> 8) / 255.0
-            a = Double(rgb & 0x000000FF) / 255.0
-        } else {
-            return nil
-        }
-        
-        self.init(.sRGB, red: r, green: g, blue: b, opacity: a)
-    }
-}
+// Note: Color(hex:) extension is defined in AssignLocationSheet.swift
 
